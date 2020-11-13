ce131880809535f3cf4d7b447565f821146c95ba
Bojan Culajevic <bc@itnouvelle.com>
2020-11-13 01:03:04 +0100

    updatind passport files

--- passport-setup.js
+++ passport-setup.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
-const keys = require('./keys')
+// const keys = require('./keys')
 const User = require('./models/User')
 
 
