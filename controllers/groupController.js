fbf083eae114d2a844b34ce9ec0dbc70a9b80b84
Bojan Culajevic <bc@itnouvelle.com>
2021-03-15 01:46:33 +0100

    display all groups on home page

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -85,7 +85,7 @@ sortByPriority = {priority:-1}
       {$group:{_id:{groupId:'$groupId'},total:{$sum:1}}},
       {$lookup:{from:'groups', localField:"_id.groupId", foreignField:"_id", as:"grupa"}},
       //izbaciti match ispod ukoliok prikazujemo sve grupe na naslovnoj stranici
-      {$match:{'grupa.frontPage':true}},
+      // {$match:{'grupa.frontPage':true}},
       {$sort:{'grupa.priority':-1}}
     ])
     // const groups = await Group.aggregate([
