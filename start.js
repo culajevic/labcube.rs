756212e8895fb344cfe99eaa56cec0bc9c10aecb
Bojan Culajevic <bc@itnouvelle.com>
2021-08-22 15:32:06 +0200

    adding ssl

--- start.js
+++ start.js
@@ -36,16 +36,17 @@ mongoose.connection.on('error', (err) => {
   console.log(`there is an error ${err}`)
 })
 
-if (process.env.NODE_ENV === 'production') {
-  const privateKey = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/privkey.pem', 'utf8');
-  const certificate = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/fullchain.pem', 'utf8');
-  const ca = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/chain.pem', 'utf8');
-  const credentials = {
-    key:privateKey,
-    cert:certificate,
-    ca:ca
-  }
-  https.createServer(credentials, app).listen(443, () => {
+if (process.env.NODE_ENV === "production") {
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/chain.pem', 'utf8');
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+
+    https.createServer(credentials, app).listen(443, () => {
         console.log('HTTPS Server running on port 443');
     });
     http.createServer(function (req, res) {
