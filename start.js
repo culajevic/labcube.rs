031aa8742c15aec2e7fd46d0f69f9efc3d1b5041
Bojan Culajevic <bc@itnouvelle.com>
2021-08-24 12:52:07 +0200

    falling back without ssl

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 const fs = require("fs")
 // const http = require('http')
 
-const https = require('https')
+// const https = require('https')
 
 // import models
 require('./models/Group')
@@ -31,37 +31,37 @@ dotenv.config({path:'variables.env'})
 const port = process.env.PORT || 1606
 
 // connect to database
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true } )
-mongoose.Promise = global.Promise
-mongoose.connection.on('error', (err) => {
-  console.log(`there is an error ${err}`)
-})
-
-if (process.env.NODE_ENV === "production") {
-    const privateKey = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/privkey.pem', 'utf8');
-    const certificate = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/cert.pem', 'utf8');
-    const ca = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/chain.pem', 'utf8');
-    const credentials = {
-        key: privateKey,
-        cert: certificate,
-        ca: ca
-    };
-
-    https.createServer(credentials, app).listen(443, () => {
-        console.log('HTTPS Server running on port 443');
-    });
-    http.createServer(function (req, res) {
-        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-        res.end();
-    }).listen(80);
-} else if (process.env.NODE_ENV === "development") {
-    app.listen(1606);
-} else {
-    app.listen(1606);
-}
+// mongoose.connect(process.env.DATABASE, { useNewUrlParser: true } )
+// mongoose.Promise = global.Promise
+// mongoose.connection.on('error', (err) => {
+//   console.log(`there is an error ${err}`)
+// })
+//
+// if (process.env.NODE_ENV === "production") {
+//     const privateKey = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/privkey.pem', 'utf8');
+//     const certificate = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/cert.pem', 'utf8');
+//     const ca = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/chain.pem', 'utf8');
+//     const credentials = {
+//         key: privateKey,
+//         cert: certificate,
+//         ca: ca
+//     };
+//
+//     https.createServer(credentials, app).listen(443, () => {
+//         console.log('HTTPS Server running on port 443');
+//     });
+//     http.createServer(function (req, res) {
+//         res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//         res.end();
+//     }).listen(80);
+// } else if (process.env.NODE_ENV === "development") {
+//     app.listen(1606);
+// } else {
+//     app.listen(1606);
+// }
 
 
 // start the app...
-// app.listen(port, ()=>{
-//   console.log(`listening on port ${port}`)
-// })
+app.listen(port, ()=>{
+  console.log(`listening on port ${port}`)
+})
