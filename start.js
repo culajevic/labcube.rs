1fbcd2d466bad6787b2d30070f1a331365e7b83f
Bojan Culajevic <bc@itnouvelle.com>
2021-08-24 12:56:13 +0200

    trying ssh

--- start.js
+++ start.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-// const fs = require("fs")
-// const http = require('http')
-
-// const https = require('https')
+const fs = require("fs")
+const http = require('http')
+const https = require('https')
 
 // import models
 require('./models/Group')
@@ -37,31 +36,31 @@ mongoose.connection.on('error', (err) => {
   console.log(`there is an error ${err}`)
 })
 //
-// if (process.env.NODE_ENV === "production") {
-//     const privateKey = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/privkey.pem', 'utf8');
-//     const certificate = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/cert.pem', 'utf8');
-//     const ca = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/chain.pem', 'utf8');
-//     const credentials = {
-//         key: privateKey,
-//         cert: certificate,
-//         ca: ca
-//     };
-//
-//     https.createServer(credentials, app).listen(443, () => {
-//         console.log('HTTPS Server running on port 443');
-//     });
-//     http.createServer(function (req, res) {
-//         res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-//         res.end();
-//     }).listen(80);
-// } else if (process.env.NODE_ENV === "development") {
-//     app.listen(1606);
-// } else {
-//     app.listen(1606);
-// }
+if (process.env.NODE_ENV === "production") {
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/labcube.rs/chain.pem', 'utf8');
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+
+    https.createServer(credentials, app).listen(443, () => {
+        console.log('HTTPS Server running on port 443');
+    });
+    http.createServer(function (req, res) {
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        res.end();
+    }).listen(80);
+} else if (process.env.NODE_ENV === "development") {
+    app.listen(1606);
+} else {
+    app.listen(1606);
+}
 
 
 // start the app...
-app.listen(port, ()=>{
-  console.log(`listening on port ${port}`)
-})
+// app.listen(port, ()=>{
+//   console.log(`listening on port ${port}`)
+// })
