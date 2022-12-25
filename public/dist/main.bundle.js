6045c1e55f917ea1c174753596d5e15457d97a7f
Bojan Culajevic <bc@itnouvelle.com>
2022-12-26 00:54:48 +0100

    header stick to top

--- public/dist/main.bundle.js
+++ public/dist/main.bundle.js
@@ -22985,7 +22985,8 @@ $(window).scroll(function () {
   var height = $(window).scrollTop();
 
   if (height > 460) {
-    $("#header > nav").addClass('fixed-top-background fixed-top'); // $(priceList).css({top:"17px",transition:'top .5s ease', positiom:'relative', zindex:'4440'})
+    $("#header > nav").addClass('fixed-top-background fixed-top');
+    $("#header").css('clip-path', 'none'); // $(priceList).css({top:"17px",transition:'top .5s ease', positiom:'relative', zindex:'4440'})
   } else {
     $("#header > nav").removeClass('fixed-top-background fixed-top');
     $(priceList).css({
