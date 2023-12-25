9d5510f59a3355dcf3864c6af7b3040d642cb718
Bojan Culajevic <bc@itnouvelle.com>
2023-12-25 12:13:14 +0100

    adding partner link

--- controllers/general.js
+++ controllers/general.js
@@ -44,6 +44,15 @@ const groupNames = await Group.find({},{name:1,slug:1,_id:0}).sort({name:1})
   })
 }
 
+exports.becomePartner = async (req,res) => {
+  const groupNames = await Group.find({},{name:1,slug:1,_id:0}).sort({name:1})
+  res.render("partners", {
+    title:"Postanite LabCube parnter",
+    groupNames,
+    user:req.user
+  })
+}
+
 exports.paymentDetails = async (req,res) => {
 const groupNames = await Group.find({},{name:1,slug:1,_id:0}).sort({name:1})
   res.render('paymentDetails',{
